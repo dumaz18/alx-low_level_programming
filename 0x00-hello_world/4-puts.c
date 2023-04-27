b85f29f7f3fe4a2f53f84cacfce9a40efe9c2211
Douae Mazirh <128824615+dumaz18@users.noreply.github.com>
2023-04-27 18:52:06 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - Using Puts Function
- *   *
- *    * Return: 0 (Success)
- *     */
+ * main - Using Puts Function
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}#include <stdio.h>
-/**
- *  * main - Using Puts Function
- *   *
- *    * Return: 0 (Success)
- *     */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
